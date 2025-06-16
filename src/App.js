ec473f01488a2c923d875bdbc8b3ab9a5e1660d4
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-06-16 20:44:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          // <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
