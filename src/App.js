88692a939d7152667b5277a9c586c04c71fc7376
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-07-02 13:19:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<About />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} /> 
           <Route path="/contact" element={<Cont />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:id" element={<BlogDetail />} />
